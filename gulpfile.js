bd851705d71410fda53aa5c43e96de5bf52f0cd0
Jan T. Sott <jan@idleberg.com>
2017-08-13 23:38:25 +0200

    modify output path

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 const cache  = require('gulp-cached');
 const debug  = require('gulp-debug');
 const gulp   = require('gulp');
-// const raster = require('gulp-raster');
+const raster = require('gulp-raster');
 const rename = require('gulp-rename');
 const svgmin = require('gulp-svgmin');
 const watch  = require('gulp-watch');
@@ -19,22 +19,22 @@ gulp.task('build:svg', gulp.series( done => {
     .pipe(cache('build:svg'))
     .pipe(debug({title: 'svgmin:'}))
     .pipe(svgmin())
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('dist/'));
 
     done();
 }));
 
-// Convert SVG to PNG
-// gulp.task('build:png', gulp.series( done => {
-//     return gulp.src(svgFiles)
-//     .pipe(cache('build:png'))
-//     .pipe(debug({title: 'raster:'}))
-//     .pipe(raster({format: 'png'}))
-//     .pipe(rename({extname: '.png'}))
-//     .pipe(gulp.dest('build/'));
+Convert SVG to PNG
+gulp.task('build:png', gulp.series( done => {
+    return gulp.src(svgFiles)
+    .pipe(cache('build:png'))
+    .pipe(debug({title: 'raster:'}))
+    .pipe(raster({format: 'png'}))
+    .pipe(rename({extname: '.png'}))
+    .pipe(gulp.dest('dist/'));
 
-//     done();
-// }));
+    done();
+}));
 
 // Watch task
 gulp.task('watch:svg', gulp.series( done => {
